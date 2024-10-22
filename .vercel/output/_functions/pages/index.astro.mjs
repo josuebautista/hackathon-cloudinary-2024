1be3e49b43b1bea3d6897fafba5dc4d888189ad9
JosueBautista <alejandrojhb84@gmail.com>
2024-10-22 17:43:23 -0600

    Delete .vercel/output directory

--- .vercel/output/_functions/pages/index.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { p as page } from '../chunks/index_Ddz-JCSN.mjs';
-export { renderers } from '../renderers.mjs';
--- .vercel/output/static/_astro/hoisted.C3Uj9Co0.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./router.Cas-jtPn.js";const t=document.querySelector("#return");t&&t.addEventListener("click",()=>{e("/")});
